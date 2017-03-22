b85fd445985a5aee381cd079162b7377f5a71a7f
Rashmi Erande <Rashmi@Rashmis-MacBook-Air.local>
2017-03-22 08:52:31 -0700

    Checking commit ranges

---
+++ python_workbook/ranges.py
@@ -0,0 +1,19 @@
+
+'''
+Create a script that generates and prints a list of numbers
+ from 1 to 20. Please do not create the list manually.
+
+
+'''
+lst1 = range(1,21)
+
+print(list(lst1))
+
+'''
+range()  is a Python built-in function that
+generates a range of integers. However, range()
+creates a Python range object. To get a real list
+object you need to use the list() function to convert
+the range object into a list object.
+
+'''
